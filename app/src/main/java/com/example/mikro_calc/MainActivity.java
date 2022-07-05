e8c94c79d18561cd0589bab8ad84b29dec2fea2f
alisa-one <72848258+alisa-one@users.noreply.github.com>
2022-07-05 17:51:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mikro_calc/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.mikro_calc;
+
+import static com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText wishes;
+    Button sendWishes1;
+    Button sendWishes2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        clickListener();
+    }
+
+    public void init() {
+        wishes = findViewById(R.id.et_wishes);
+        sendWishes1 = findViewById(R.id.btn_send1);
+        sendWishes2 = findViewById(R.id.btn_send2);
+    }
+    public void clickListener() {
+        sendWishes1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendWishes1.setVisibility(View.INVISIBLE);
+                wishes.setVisibility(View.VISIBLE);
+                wishes.setCursorVisible(true);
+                sendWishes2.setVisibility(View.VISIBLE);
+            }
+        });
+        sendWishes2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  sendEmail();
+            }
+        });
+    }
+
+    @SuppressLint("IntentReset")
+    protected void sendEmail() {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My wishes before space flight");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, wishes.getText().toString());
+        Log.e("TAG", "!! Function sendEmail run");
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finish", "!! LetterApps   open for send email");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.",
+                   Toast.LENGTH_SHORT).show();
+                  }
+    }
+}
+
+
+
+
+
+
+
